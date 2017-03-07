656e3325a8caecb5ceca047f6d3d413d2efea1c6
Kirill Belov <KirillBelovTest@gmail.com>
2017-03-07 10:17:03 +0400

    added paclet info file

---
+++ PacletInfo.m
@@ -0,0 +1,17 @@
+(* ::Package:: *)
+
+(* Paclet Info File *)
+
+Paclet[
+    Name -> "NonStandartImportExport",
+    Version -> "0.0.3",
+    MathematicaVersion -> "10.0+",
+    Description -> "Support import and export non standart files",
+    Creator -> "Anton Ekimenko, Kirill Belov",
+    Extensions -> 
+        {
+            {"Kernel", Root -> ".", Context -> "NonStandartImportExport`"}
+        }
+]
+
+
