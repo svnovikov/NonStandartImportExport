3cfee938ba7a9d61907732c3d422064d33bc624b
Kirill Belov <KirillBelovTest@gmail.com>
2017-03-15 01:16:10 +0400

    update

--- NonStandartImportExport/NonStandartImportExport.m
+++ NonStandartImportExport/NonStandartImportExport.m
@@ -8,80 +8,80 @@
 (* :Email: KirillBelovTest@gmail.com *)
 (* :Creator: Kirill Belov *)
 
-BeginPackage["NonStandartImportExport`", 
+BeginPackage["NonStandartImportExport`",
 	{
 		(* SEG-Y *)
-		"NonStandartImportExport`Extensions`SEGY`", 
+		"NonStandartImportExport`Extensions`SEGY`",
 		"FileFormats`SEGY`"
 	}
-]; 
+];
 
 (* preparation *)
-Unprotect["`*"]; 
-Clear["`*"]; 
+Unprotect["`*"];
+Clear["`*"];
 
-$NonStandartImportExportDirectory::usage = 
-"$NonStandartImportExportDirectory - NonStandartImportExamport project directory"; 
+$NonStandartImportExportDirectory::usage =
+"$NonStandartImportExportDirectory - NonStandartImportExamport project directory";
 
-NonStandartLoad::usage = 
-"NonStandartLoad[Unload[]]"; 
+NonStandartLoad::usage =
+"NonStandartLoad[Unload[]]";
 
-NonStandartImport::usage = 
-"NonStandartImport[file, extension]"; 
+NonStandartImport::usage =
+"NonStandartImport[file, extension]";
 
-NonStandartExport::usage = 
-"NonStandartImport[file, data, extension]"; 
+NonStandartExport::usage =
+"NonStandartImport[file, data, extension]";
 
-Begin["`Private`"]; 
+Begin["`Private`"];
 
-NonStandartImport::nofile = 
-"File `1` not found during non standart import"; 
+NonStandartImport::nofile =
+"File `1` not found during non standart import";
 
-NonStandartImport::wrgfl = 
-"The wrong file `1`"; 
+NonStandartImport::wrgfl =
+"The wrong file `1`";
 
-NonStandartExport::wrgfl = 
-"The wrong file `1`"; 
+NonStandartExport::wrgfl =
+"The wrong file `1`";
 
 (* definition for the project directory *)
-$NonStandartImportExportDirectory = 
-FileNameJoin[FileNameSplit[$InputFileName][[1 ;; -3]]]; 
+$NonStandartImportExportDirectory =
+FileNameJoin[FileNameSplit[$InputFileName][[1 ;; -3]]];
 
-(* SEG-Y *) 
+(* SEG-Y *)
 
-SetAttributes[NonStandartImport, {Listable}]; 
+SetAttributes[NonStandartImport, {Listable}];
 
-NonStandartImport[file: (_File | _String), "SEGY" | "SGY", opts: OptionsPattern[SEGYImport]] /; 
+NonStandartImport[file: (_File | _String), "SEGY" | "SGY", opts: OptionsPattern[SEGYImport]] /;
 If[
-	FileExistsQ[file], 
-	True, 
+	FileExistsQ[file],
+	True,
 	If[
-		StringMatchQ[FileExtension[file], "sgy" | "segy", IgnoreCase -> True], 
-		Message[NonStandartImport::wrgfl, file]; False, 
+		StringMatchQ[FileExtension[file], "sgy" | "segy", IgnoreCase -> True],
+		Message[NonStandartImport::wrgfl, file]; False,
 		Message[NonStandartImport::nofile, file]; False
 	]
-] := 
+] :=
 SEGYImport[file, opts];
 
-NonStandartExport[file: (_File | _String), data_SEGYData, "SEGY" | "SGY", opts: OptionsPattern[SEGYExport]] /; 
+NonStandartExport[file: (_File | _String), data_SEGYData, "SEGY" | "SGY", opts: OptionsPattern[SEGYExport]] /;
 If[
-	StringMatchQ[FileExtension[file], "sgy" | "segy", IgnoreCase -> True], 
-	True, 
+	StringMatchQ[FileExtension[file], "sgy" | "segy", IgnoreCase -> True],
+	True,
 	Message[NonStandartExport::wrgfl, file]; False
-] := 
-SEGYExport[file, data, opts]; 
+] :=
+SEGYExport[file, data, opts];
 
-SetAttributes[NonStandartLoad, {Listable}]; 
+SetAttributes[NonStandartLoad, {Listable}];
 
-NonStandartLoad[unloaded_SEGYUnloaded, opts: OptionsPattern[SEGYLoad]] := 
-SEGYLoad[unloaded, opts]; 
+NonStandartLoad[unloaded_SEGYUnloaded, opts: OptionsPattern[SEGYLoad]] :=
+SEGYLoad[unloaded, opts];
 
-(* /SEG-Y *) 
+(* /SEG-Y *)
 
-End[]; (*`Private`*) 
+End[]; (*`Private`*)
 
-(* protection from change *) 
-SetAttributes[Evaluate[Names["`*"]], {ReadProtected}]; 
-Protect["`*"]; 
+(* protection from change *)
+SetAttributes[Evaluate[Names["`*"]], {ReadProtected}];
+Protect["`*"];
 
-EndPackage[]; (*NonStandartImportExport`*)  
+EndPackage[]; (*NonStandartImportExport`*)
